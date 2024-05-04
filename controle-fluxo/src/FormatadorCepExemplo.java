ce903cad63cdadaa0c8fd9a5e9017b53338fcf9e
GABRIEL EDUARDO JOIOSO <g.eduardojoioso2@dxc.com>
2024-05-04 14:56:20 -0300

    Add FormatarCepExemplo

--- controle-fluxo/src/FormatadorCepExemplo.java
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -1,11 +1,24 @@
+
+// Definição da classe de exceção para CEP inválido
+class CepInvalidoException extends Exception {
+    // Construtor padrão sem argumentos
+    public CepInvalidoException() {
+        super("CEP inválido!");
+    }
+
+    // Construtor com uma mensagem personalizada
+    public CepInvalidoException(String message) {
+        super(message);
+    }
+}
+
 public class FormatadorCepExemplo {
     public static void main(String[] args) {
         try {
-            String cepFormatado = formatarCep("23765064");
+            String cepFormatado = formatarCep("237");
             System.out.println(cepFormatado);
-            
         } catch (CepInvalidoException e) {
-            System.out.println("O CEP não corresponde com as regras de negocio!");
+            System.out.println("O CEP não corresponde com as regras de negócio!");
         }
     }
 
@@ -13,5 +26,7 @@ public class FormatadorCepExemplo {
         if(cep.length() != 8){
             throw new CepInvalidoException();
         }
+        // Retorna o CEP formatado se estiver correto
+        return cep.substring(0, 5) + "-" + cep.substring(5);
     }
 }
